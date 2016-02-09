185fd434a385c770505c7b7114fd8eec614c76ed
Evan Prodromou <evan@fuzzy.io>
2016-02-09 09:09:50 -0500

    License and license notifications in README and source files

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# activitystreams-validator
+activitystreams-validator
+=========================
+
 validator for AS2 URLs and uploaded data
+
+License
+-------
+
+Copyright © 2016 World Wide Web Consortium, (Massachusetts Institute of
+Technology, European Research Consortium for Informatics and Mathematics, Keio
+University, Beihang). All Rights Reserved. This work is distributed under the
+[W3C® Software License](http://www.w3.org/Consortium/Legal/copyright-software)
+in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+Installation
+------------
--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+// activitystreams-validator: https://github.com/w3c-social/activitystreams-validator
+//
+// Copyright © 2016 World Wide Web Consortium, (Massachusetts Institute of
+// Technology, European Research Consortium for Informatics and Mathematics,
+// Keio University, Beihang). All Rights Reserved. This work is distributed
+// under the W3C® Software License [1] in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.
+// 
+// [1] http://www.w3.org/Consortium/Legal/copyright-software
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
