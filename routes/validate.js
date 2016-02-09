4d8cf0a3b0c42658d89ffc23bb28075023ee9201
Evan Prodromou <evan@fuzzy.io>
2016-02-09 11:30:08 -0500

    Basic validator for form data

--- routes/validate.js
+++ routes/validate.js
@@ -10,7 +10,9 @@
 // [1] http://www.w3.org/Consortium/Legal/copyright-software
 
 var express = require('express');
-var upload = require('multer')({dest: process.env['UPLOADS'] || '/tmp/uploads'})
+var upload = require('multer')({dest: process.env['UPLOADS'] || '/tmp/uploads'});
+var as = require('activitystrea.ms');
+
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
@@ -31,10 +33,24 @@ router.post('/', function(req, res, next) {
     if (!req.body || !req.body.data) {
       return next(new Error("No data"));
     }
-    // Parse req.body.data
-    // Validate req.body.data
-    // Output HTML validation report
-    res.send("Validation report for data");
+    try {
+      var input = JSON.parse(req.body.data);
+    } catch (err) {
+      return next(err);
+    }
+    as.import(input, function(err, results) {
+      if (err) {
+        next(err);
+      } else {
+        results.prettyWrite(function(err, output) {
+          if (err) {
+            next(err);
+          } else {
+            res.render("validate", {title: "Validation Report", input: req.body.data, output: output});
+          }
+        });
+      }
+    });
   } else if (req.is('multipart')) {
     upload.single('file')(req, res, function(err) {
       if (err) {
